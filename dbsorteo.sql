b61e5a7f432727bc3ba9e7ee1b26a404ecb60593
Maynor Aguilera <113657563+maguilera4323@users.noreply.github.com>
2023-01-29 18:26:32 -0600

    cambios bd

--- dbsorteo.sql
+++ dbsorteo.sql
@@ -25,12 +25,18 @@ CREATE TABLE IF NOT EXISTS `boletos` (
   `id_usuario` int(11) NOT NULL,
   `id_sorteo` int(11) NOT NULL,
   `numero_boleto` int(4) NOT NULL,
-  `fecha_compra` date NOT NULL,
+  `fecha_compra` datetime NOT NULL,
   PRIMARY KEY (`id_boleto`) USING BTREE,
-  KEY `IdUsuario` (`id_usuario`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `IdUsuario` (`id_usuario`) USING BTREE,
+  KEY `Índice 3` (`id_sorteo`),
+  CONSTRAINT `FK_boletos_sorteos` FOREIGN KEY (`id_sorteo`) REFERENCES `sorteos` (`id_sorteo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_boletos_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.boletos: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla dbsorteo.boletos: ~2 rows (aproximadamente)
+INSERT INTO `boletos` (`id_boleto`, `id_usuario`, `id_sorteo`, `numero_boleto`, `fecha_compra`) VALUES
+	(1, 4, 1, 1, '2023-01-29 16:35:14'),
+	(2, 5, 1, 2, '2023-01-29 17:49:45');
 
 -- Volcando estructura para tabla dbsorteo.boletos_adquiridos
 CREATE TABLE IF NOT EXISTS `boletos_adquiridos` (
@@ -49,13 +55,12 @@ CREATE TABLE IF NOT EXISTS `empresas` (
   `telefono` varchar(15) DEFAULT NULL,
   `correo_electronico` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id_empresa`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.empresas: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla dbsorteo.empresas: ~1 rows (aproximadamente)
 INSERT INTO `empresas` (`id_empresa`, `nombre_empresa`, `direccion`, `telefono`, `correo_electronico`) VALUES
-	(1, 'SULA', 'Col. Victor F. Ardón, Tegucigalpa M.D.C', '12345678', 'ventas@sula.hn'),
-	(2, 'PIZZA HUT', 'COL. TEPEYAC', '22222222', 'pizzahut@pizza.hn'),
-	(4, 'MENDELS', 'sps', '12345677', 'soporte@mendels.hn');
+	(1, 'SULA', 'Col. Victor F. Ardon, Tegucigalpa M.D.C', '22021294', 'contacto@sula.hn'),
+	(2, 'CINEMARK', 'Res. Las Uvas', '22091298', 'soporte@cinemark.hn');
 
 -- Volcando estructura para tabla dbsorteo.modulos
 CREATE TABLE IF NOT EXISTS `modulos` (
@@ -128,40 +133,31 @@ CREATE TABLE IF NOT EXISTS `personas` (
   `modificado_por` varchar(30) DEFAULT NULL,
   `fecha_modificacion` datetime DEFAULT NULL,
   PRIMARY KEY (`id_persona`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.personas: ~15 rows (aproximadamente)
+-- Volcando datos para la tabla dbsorteo.personas: ~5 rows (aproximadamente)
 INSERT INTO `personas` (`id_persona`, `nombres`, `apellidos`, `dni`, `telefono`, `sexo`, `direccion`, `creado_por`, `fecha_creacion`, `modificado_por`, `fecha_modificacion`) VALUES
-	(1, 'PABLO', 'SEVILLA', '1203199623456', '99999999', 'Masculino', 'Col. Nueva Suyapa, sector 2, casa 17. Tegucigalpa M.D.C', 'admin', '2023-01-28 13:39:04', NULL, NULL),
-	(2, 'jkhkasdjh', 'hkjhkjhjk', '111111111111', '1111111', 'Masculino', 'jhgjhghkggggjh', 'admin', '2023-01-28 14:51:27', NULL, NULL),
-	(3, 'MARIA', 'SALGADO', '0801199812456', '99999999', 'Femenino', 'no se crack', 'prueba', '2023-01-28 15:33:56', NULL, NULL),
-	(4, 'jhghkhjkhjk', 'hjkhkjhjk', '897897797', '78977789', 'Masculino', 'jhgjhgjhgggjh', 'admin', '2023-01-28 15:35:46', NULL, NULL),
-	(5, 'hvvjhvj', 'jhvjhvjhh', '878678678', '786786678', 'Masculino', 'jkhhjkhjkhjk', 'admin', '2023-01-28 15:36:10', NULL, NULL),
-	(6, 'ANTONY', 'MARK', '8888888888888', '11111111', 'Femenino', 'jhjjgjgjgjh', 'prueba', '2023-01-28 15:37:05', NULL, NULL),
-	(7, 'guigig', 'igigigi', '7896789789', '978978978', 'Masculino', '78978978979', '987897', '2023-01-28 15:40:25', NULL, NULL),
-	(8, 'gghjh', 'hjkhjk', '789789', '7897897', 'Masculino', 'jhjjkhjk', 'yuui', '2023-01-28 15:40:50', NULL, NULL),
-	(9, 'GFHFGFJH', 'GGGGJHG', '786666666666', '11111111', 'Masculino', 'ghfghfghfghfh', 'prueba', '2023-01-28 15:41:42', NULL, NULL),
-	(10, 'kbmnbbk', 'jknbnb', 'jnbmnb', 'mnbmnbmnbmn', 'Masculino', 'mbnmnbmn', 'bnvbnvbn', '2023-01-28 15:45:09', NULL, NULL),
-	(11, 'FSDFSFS', 'SDFSDFSD', '3534534534', '3123123123', 'Femenino', 'fvgvsvsdvd', 'prueba', '2023-01-28 15:46:25', NULL, NULL),
-	(12, 'FSDFSFS', 'SDFSDFSD', '3534534534', '3123123123', 'Femenino', 'fvgvsvsdvd', 'prueba', '2023-01-28 15:47:18', NULL, NULL),
-	(13, 'GFSFSDFFFSDFSD', 'SDFSDFSD', '3534534534', '3123123123', 'Femenino', 'fvgvsvsdvd', 'prueba', '2023-01-28 15:48:22', NULL, NULL),
-	(14, 'MARINA', 'DIAMANDIS', '0801199719345', '11111111', 'Femenino', 'jkhjkhjkh', 'prueba', '2023-01-28 15:58:28', NULL, NULL),
-	(15, 'JORGE', 'ASDFG', '1234567890111', '12345678', 'Masculino', 'Col. San Jose del Carmen, Tegucigalpa M.D.C', 'prueba', '2023-01-28 16:01:58', NULL, NULL);
+	(1, 'PRUEBA', 'USUARIOS', '80819991034', '98762301', 'Masculino', 'Col. San Angel, Tegucigalpa M.D.C', 'prueba', '2023-01-29 16:02:09', NULL, NULL),
+	(2, 'TAYLOR', 'SWIFT', '0701238712845', '97652301', 'Femenino', 'USA', 'prueba', '2023-01-29 16:04:59', NULL, NULL),
+	(3, 'SERJ', 'TANKIAN', '1208199790012', '33120973', 'Masculino', 'Col. Arturo Quezada, zona 3, casa 17, bloque 12. Tegucigalpa M.D.C', 'prueba', '2023-01-29 16:07:07', NULL, NULL),
+	(4, 'JUAN ANDRÉS', 'PEREZ ZEPEDA', '0610198923081', '99903456', 'Masculino', 'Residencial Plaza, Tegucigalpa M.D.C', 'prueba', '2023-01-29 16:13:01', NULL, NULL),
+	(5, 'WILSON', 'RIOS', '1202198500092', '32412679', 'Masculino', 'Col. Las Acacias, Arizona, Atlántida', 'prueba', '2023-01-29 17:46:50', NULL, NULL);
 
 -- Volcando estructura para tabla dbsorteo.premios
 CREATE TABLE IF NOT EXISTS `premios` (
   `id_premio` int(11) NOT NULL AUTO_INCREMENT,
   `id_sorteo` int(11) NOT NULL,
   `nombre_premio` varchar(300) NOT NULL,
+  `cantidad_disponible` int(11) DEFAULT NULL,
   `foto_premio` varchar(300) NOT NULL,
   PRIMARY KEY (`id_premio`) USING BTREE,
-  KEY `Índice 2` (`id_sorteo`) USING BTREE,
-  CONSTRAINT `FK_premios_sorteos` FOREIGN KEY (`id_sorteo`) REFERENCES `sorteos` (`id_sorteo`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  KEY `Índice 2` (`id_sorteo`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.premios: ~1 rows (aproximadamente)
-INSERT INTO `premios` (`id_premio`, `id_sorteo`, `nombre_premio`, `foto_premio`) VALUES
-	(1, 2, 'CASA', '../vistas/assets/premios/1f3e0.png');
+-- Volcando datos para la tabla dbsorteo.premios: ~2 rows (aproximadamente)
+INSERT INTO `premios` (`id_premio`, `id_sorteo`, `nombre_premio`, `cantidad_disponible`, `foto_premio`) VALUES
+	(1, 1, 'CAMISA HOMBRE', 2, '../vistas/assets/premios/camisa.jpg'),
+	(2, 1, 'JUGOS', 80, '../vistas/assets/premios/jugo-naranja.jpg');
 
 -- Volcando estructura para tabla dbsorteo.premios_sorteo
 CREATE TABLE IF NOT EXISTS `premios_sorteo` (
@@ -173,9 +169,7 @@ CREATE TABLE IF NOT EXISTS `premios_sorteo` (
   PRIMARY KEY (`id_premio_sorteo`) USING BTREE,
   KEY `IdSorteo` (`id_sorteo`) USING BTREE,
   KEY `IdUsuario` (`id_usuario`) USING BTREE,
-  KEY `FK_premios_sorteo_premios` (`id_premio`),
-  CONSTRAINT `FK_premios_sorteo_premios` FOREIGN KEY (`id_premio`) REFERENCES `premios` (`id_premio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_premios_sorteo_sorteos` FOREIGN KEY (`id_sorteo`) REFERENCES `sorteos` (`id_sorteo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK_premios_sorteo_premios` (`id_premio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla dbsorteo.premios_sorteo: ~0 rows (aproximadamente)
@@ -196,26 +190,24 @@ CREATE TABLE IF NOT EXISTS `roles` (
 INSERT INTO `roles` (`id_rol`, `rol`, `descripcion`, `creado_por`, `fecha_creacion`, `modificado_por`, `fecha_modificacion`) VALUES
 	(1, 'ADMINISTRADOR', 'Administrador del sistema', 'prueba', '2023-01-18 20:42:58', NULL, NULL),
 	(2, 'EMPLEADO', 'Encargado de monitorear actividad de usuarios', 'prueba', '2023-01-19 07:42:09', NULL, NULL),
-	(3, 'PARTICIPANTE', 'user', 'prueba', '2023-01-22 12:06:58', 'prueba', '2023-01-24 22:38:19');
+	(3, 'PARTICIPANTE', 'Usuario registrado en el sistema que participa de los sorteos realizados', 'prueba', '2023-01-22 12:06:58', 'TANKIAN', '2023-01-29 16:11:10');
 
 -- Volcando estructura para tabla dbsorteo.sorteos
 CREATE TABLE IF NOT EXISTS `sorteos` (
   `id_sorteo` int(11) NOT NULL AUTO_INCREMENT,
   `id_empresa` int(11) NOT NULL,
   `nombre_sorteo` varchar(100) NOT NULL,
-  `fecha_realizacion` date NOT NULL,
+  `fecha_realizacion` datetime NOT NULL,
   `cantidad_boletos` int(10) DEFAULT NULL,
   `estado_sorteo` enum('Pendiente','Realizado') DEFAULT NULL,
   PRIMARY KEY (`id_sorteo`) USING BTREE,
-  KEY `IdEmpresa` (`id_empresa`) USING BTREE,
-  CONSTRAINT `FK_sorteos_empresas` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id_empresa`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  KEY `IdEmpresa` (`id_empresa`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.sorteos: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla dbsorteo.sorteos: ~1 rows (aproximadamente)
 INSERT INTO `sorteos` (`id_sorteo`, `id_empresa`, `nombre_sorteo`, `fecha_realizacion`, `cantidad_boletos`, `estado_sorteo`) VALUES
-	(1, 2, '1ER AÑO INTERNACIONAL DE LA PIZZA', '2023-01-28', 100, 'Pendiente'),
-	(2, 1, 'MARATON SULA 1 EDICION', '2023-02-16', 1200, 'Realizado'),
-	(4, 4, 'ROPA LOKA', '2023-01-26', 150, 'Pendiente');
+	(1, 1, '1ER MARATON DE VERANO SULA', '2023-02-02 21:30:00', 120, 'Pendiente'),
+	(2, 2, 'PREMIERE UNA LOCA NAVIDAD CATRACHA 2', '2023-02-08 21:00:00', 50, 'Pendiente');
 
 -- Volcando estructura para tabla dbsorteo.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -234,22 +226,17 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   PRIMARY KEY (`id_usuario`) USING BTREE,
   KEY `FK_empleados_roles` (`id_rol`) USING BTREE,
   KEY `FK_usuarios_personas` (`id_persona`),
+  CONSTRAINT `FK_usuarios_personas` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_usuarios_roles` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla dbsorteo.usuarios: ~11 rows (aproximadamente)
+-- Volcando datos para la tabla dbsorteo.usuarios: ~5 rows (aproximadamente)
 INSERT INTO `usuarios` (`id_usuario`, `id_persona`, `usuario`, `estado`, `contrasena`, `id_rol`, `fecha_ultima_conexion`, `correo_electronico`, `creado_por`, `fecha_creacion`, `modificado_por`, `fecha_modificacion`) VALUES
-	(1, 1, 'prueba', 'Activo', '$2y$12$i836qbF5sxjT0I2VHP13XeLQ7ZdbVu28cmy1bKjsxQUM9mk6aBV/S', 1, NULL, 'admin@admin.com', 'admin', '2023-01-28 13:40:48', NULL, NULL),
-	(2, 2, 'adasd', 'Inactivo', 'fsdfsdfsd', 1, NULL, 'adasdas@dasd', 'admin', '2023-01-28 14:51:57', NULL, NULL),
-	(5, 3, 'sdfsdfsdf', 'Activo', 'zvxcvxcvxcvxcvxc', 1, NULL, 'vbdfbdfbdfb', 'admin', '0000-00-00 00:00:00', NULL, NULL),
-	(7, 6, 'hjgjhgjh', 'Inactivo', 'jjhgghgjhgjh', 1, NULL, 'uytgjhgjhg', 'admin', '2023-01-28 15:38:24', NULL, NULL),
-	(8, 2, 'ghvghjhvjh', 'Activo', 'ugjgjhgjhgjh', 1, NULL, 'hgfghjhg', 'admin', '0000-00-00 00:00:00', NULL, NULL),
-	(10, NULL, 'hjkhkh', 'Activo', 'kljkljkljkljkljkl', 1, NULL, 'jljkjkljkljkl', 'jkkljkl', '2023-01-28 15:44:23', NULL, NULL),
-	(11, 0, 'ASFSDFSD', 'Activo', '$2y$12$wMX7B9DQrgYgNxEv8pHgGOmsU5k4uM9lfpJHrpd1XTGYD/sCcvsC.', 1, NULL, 'vxvxvxc@fdfsdf.com', 'prueba', '2023-01-28 15:46:25', NULL, NULL),
-	(12, 0, 'JHGJHGJKGH', 'Activo', '$2y$12$wgmDNr.ViDRwg8fus8HWNOaHS.AnsXclHOvBp42pOxpo8gBVKUmhq', 1, NULL, 'gjhgjgjg@fdfsdf.com', 'prueba', '2023-01-28 15:47:18', NULL, NULL),
-	(13, 13, 'UUTYUTY', 'Activo', '$2y$12$qJxtXsQbZmmS5NFXD91CPuvIAFE/w/r9B703uSkSx69E1LoCb21l2', 1, NULL, 'kjjljklkjk@fdfsdf.com', 'prueba', '2023-01-28 15:48:22', NULL, NULL),
-	(14, 14, 'MARINA', 'Activo', '$2y$12$sJFtDzy5M6K9EFnaUHAKBOytG5m5qS59yjWkAyCf374lnzXCg5.iS', 1, NULL, 'marina@gmail.com', 'prueba', '2023-01-28 15:58:28', NULL, NULL),
-	(15, 15, 'CURIOSOJORGE', 'Eliminado', '$2y$12$GjQ2qNVgjwk.TZd/hovPTOUQSod2l9bZOWn1VhntiyfoC3Y3RMT8C', 2, NULL, 'curiosojorge@gmail.com', 'prueba', '2023-01-28 16:01:58', NULL, NULL);
+	(1, 1, 'prueba', 'Activo', '$2y$12$i836qbF5sxjT0I2VHP13XeLQ7ZdbVu28cmy1bKjsxQUM9mk6aBV/S', 1, NULL, 'prueba@prueba.com', 'prueba', '2023-01-29 16:03:30', NULL, NULL),
+	(2, 2, 'SWIFTIE', 'Activo', '$2y$12$DfRsD1/Qf1FisI/VwQWrve5iZ2GboPhH7BCtLloE5p53q1S8aqmB.', 2, NULL, 'swift@taylor.com', 'prueba', '2023-01-29 16:04:59', NULL, NULL),
+	(3, 3, 'TANKIAN', 'Activo', '$2y$12$TypNeC35UbRFM0wMYfpi9.WapmQiOvmDPv6Fymktb/W4jfxf0TucG', 1, NULL, 'serjtankian@gmail.es', 'prueba', '2023-01-29 16:07:07', NULL, NULL),
+	(4, 4, 'JUANP', 'Activo', '$2y$12$i836qbF5sxjT0I2VHP13XeLQ7ZdbVu28cmy1bKjsxQUM9mk6aBV/S', 3, NULL, 'juanperez@hotmail.com', 'prueba', '2023-01-29 16:13:30', NULL, NULL),
+	(5, 5, 'RIOSWIL', 'Activo', '$2y$12$DfRsD1/Qf1FisI/VwQWrve5iZ2GboPhH7BCtLloE5p53q1S8aqmB.', 3, NULL, 'ordonezwilson@outlook.com', 'prueba', '2023-01-20 12:34:41', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
