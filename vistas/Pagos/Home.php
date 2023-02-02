04978a7fa601a0bfd58a8c66539f567a21282eb3
Elvinunah <elvin.macoto@unah.hn>
2023-02-02 12:57:39 -0600

    Resolucion conflictos

---
+++ vistas/Pagos/Home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+     <!-- Replace "test" with your own sandbox Business account app client ID -->
+     <script src="https://www.paypal.com/sdk/js?client-id=AY8ZdCX_D1xBvYT5-ThAjwKHC26ECXTReASlTO98lkYJCy5CscrlwE3bXKwS1VR_L1XtYopUOQVT3Pfm&currency=USD"></script>
+</head>
+<body>
+   
+
+    <div id="paypal-button-container">
+        
+    </div>
+
+    <script>
+        paypal.Buttons().render('#paypal-button-container');
+    </script>
+
+    
+</body>
+</html>
